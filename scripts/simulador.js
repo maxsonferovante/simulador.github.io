e051f61dffb66d6bc422d2dc2f0b527464fbdaad
maxsonferovante <maxsonferovante@gmail.com>
2021-03-17 21:44:03 -0300

    pwa-app agora vai dar certo

---
+++ scripts/simulador.js
@@ -0,0 +1,44 @@
+var ref_atual = [
+        30,
+        4,
+        0,
+        165,
+        14,
+        12,
+        26,
+        23,
+        84,
+        6,
+        6,
+        0,
+        0,
+        4,
+        0,
+        0,
+        0
+    ],
+    ref_alterada = [
+        30,
+        4,
+        0,
+        165,
+        21,
+        18,
+        3,
+        3,
+        76,
+        8,
+        8,
+        8,
+        7,
+        4,
+        0,
+        0,
+        0
+    ]
+
+function atual() {  
+    }  
+function alterada() {  
+        alert("Welcome to the javaTpoint.com");  
+        }  
